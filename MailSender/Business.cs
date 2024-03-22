7a33c68c44c277120a9d33cb3eb1b3869e0d8872
GustavoSantosBA <95386548+GustavoSantosBA@users.noreply.github.com>
2024-03-22 17:34:27 -0300

    Adicionar arquivos de projeto.

---
+++ MailSender/Business.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("MailSender")]
+    [ComVisible(true)]
+    public class Business
+    {
+        public string Host { get; set; }
+        public int Porta { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public string Remetente { get; set; }
+        public string Destinatarios { get; set; }
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+        public string Anexos { get; set; }
+        public string SendEmail()
+        {
+            string resultado = "";
+
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                SmtpClient client = new SmtpClient();
+                client.Host = Host;
+                client.Port = Porta;
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(Usuario, Senha);
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                //client.UseDefaultCredentials = true;
+                MailMessage mail = new MailMessage();
+                mail.Sender = new MailAddress(Remetente, Assunto);
+                mail.From = new MailAddress(Remetente, Assunto);
+
+                if (Anexos != null)
+                {
+                    foreach (var item in Anexos.Split(';'))
+                    {
+                        mail.Attachments.Add(new Attachment(item));
+                    }
+                }
+
+                foreach (var item in Destinatarios.Split(';'))
+                {
+                    mail.To.Add(new MailAddress(item));
+                }
+
+                mail.Subject = Assunto;
+                mail.Body = Mensagem;
+                mail.IsBodyHtml = true;
+                mail.Priority = MailPriority.High;
+                //
+                client.Send(mail);
+                resultado = "Email enviado com sucesso!";
+            }
+            catch (Exception ex)
+            {
+                resultado = $"Falha ao enviar e-mail {ex.Message} {ex.InnerException}";
+            }
+            return resultado;
+        }
+    }
+}
