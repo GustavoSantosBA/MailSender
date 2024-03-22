433a89e455a3854fae0712ffcbb45094fc008d7a
GustavoSantosBA <95386548+GustavoSantosBA@users.noreply.github.com>
2024-03-22 18:33:45 -0300

    Dll para envio de E-mail

--- MailSender/Business.cs
+++ MailSender/Business.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using System.Net;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace MailSender
 {
